e7ca306feb52f336431f208466e0401a61efe571
Mihailo Lukic <lukicmihailo@gmail.com>
2014-02-23 21:32:22 +0100

    Dodao fajl za pokretanje

---
+++ pokretanjeAplikacije.sh
@@ -0,0 +1,18 @@
+mkdir MasterRad
+cd MasterRad
+git clone https://github.com/lukicmihailo/Projekat
+#wget "http://dl.google.com/android/ndk/android-ndk-r8c-linux-x86.tar.bz2"
+#wget "http://dl.google.com/android/android-sdk_r21.0.1-linux.tgz" 
+#export ANDROIDSDK="/home/mihailo/conf/android-sdk-linux"
+#export ANDROIDNDK="/home/mihailo/conf/android-ndk-r8c"
+#export ANDROIDNDKVER=r8c
+#export ANDROIDAPI=14
+#export PATH=$PATH:/home/mihailo/conf/android-sdk-linux/platform-tools
+#export PATH=$PATH:/home/mihailo/conf/android-sdk-linux/tools
+#git clone git://github.com/kivy/python-for-android
+#cd python-for-android 
+#./distribute.sh -m "openssl pil kivy"
+#cd dist/default
+#./build.py --package org.umlpainter --name umlpainter \ --version 1.0 --dir /home/mihailo/MasterRad/Projekat debug
+#adb install bin/umlpainter-1.0-debug.apk
+
