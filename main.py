4fe4559b10fee3c9bffd0854d8e67cbcea4751d7
Mihailo Lukic <lukicmihailo@gmail.com>
2014-03-01 18:47:02 +0100

    Dodao izmenu batch fajla za pokretanje

--- main.py
+++ main.py
@@ -7,26 +7,8 @@ Created on Feb 22, 2014
 import kivy
 kivy.require('1.0.6')
 
-from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.label import Label
-from kivy.graphics import Color, Rectangle, Point, GraphicException
-from random import random
-from math import sqrt
 
-class UMLPainter(App):
-    title = 'UMLPainter'
 
-    def build(self):
-	print("Hello world")
-        return True
-
-    def on_pause(self):
-        return True
-	
-    def run(self):
-	print("Hello world")
-	return True
 
 if __name__ == '__main__':
-      UMLPainter().run()
+     print("Radi hello")
--- pokretanjeAplikacije.sh
+++ pokretanjeAplikacije.sh
@@ -1,18 +1,25 @@
+echo "************Kreiranje prozora************"
 mkdir MasterRad
 cd MasterRad
+echo "************Preuzimanje UmlPainter-a************"
 git clone https://github.com/lukicmihailo/Projekat
 #wget "http://dl.google.com/android/ndk/android-ndk-r8c-linux-x86.tar.bz2"
 #wget "http://dl.google.com/android/android-sdk_r21.0.1-linux.tgz" 
-#export ANDROIDSDK="/home/mihailo/conf/android-sdk-linux"
-#export ANDROIDNDK="/home/mihailo/conf/android-ndk-r8c"
-#export ANDROIDNDKVER=r8c
-#export ANDROIDAPI=14
-#export PATH=$PATH:/home/mihailo/conf/android-sdk-linux/platform-tools
-#export PATH=$PATH:/home/mihailo/conf/android-sdk-linux/tools
-#git clone git://github.com/kivy/python-for-android
-#cd python-for-android 
-#./distribute.sh -m "openssl pil kivy"
-#cd dist/default
-#./build.py --package org.umlpainter --name umlpainter \ --version 1.0 --dir /home/mihailo/MasterRad/Projekat debug
-#adb install bin/umlpainter-1.0-debug.apk
+echo "************Podesavanje varijabli************"
+export ANDROIDSDK="/home/mihailo/conf/android-sdk-linux"
+export ANDROIDNDK="/home/mihailo/conf/android-ndk-r8c"
+export ANDROIDNDKVER=r8c
+export ANDROIDAPI=14
+export PATH=$PATH:/home/mihailo/conf/android-sdk-linux/platform-tools
+export PATH=$PATH:/home/mihailo/conf/android-sdk-linux/tools
+echo "************Preuzimanje python for android************"
+git clone git://github.com/kivy/python-for-android
+cd python-for-android 
+echo "************Instalacija pythona for android************"
+./distribute.sh -m "openssl pil kivy"
+cd dist/default
+echo "************Build aplikacije************"
+./build.py --package org.umlpainter --name UmlPainter \--version 1.0 --dir ~/MasterRad/Projekat debug
+echo "Instalacija aplikacije na mobilni telefon"
+adb install bin/UmlPainter-1.0-debug.apk
 
