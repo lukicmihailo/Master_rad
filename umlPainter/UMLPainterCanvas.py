56dc284d0f5d4fb8550e0c2b0c7d4b8cf53f7b2a
Mihailo Lukic <lukicmihailo@gmail.com>
2014-03-23 01:21:15 +0100

    Donekle odvojio reagovanje na klik na kanvasu i klik na dugmice

--- umlPainter/UMLPainterCanvas.py
+++ umlPainter/UMLPainterCanvas.py
@@ -4,10 +4,9 @@ Created on Mar 2, 2014
 @author: mihailo
 '''
 import kivy
-from kivy.graphics import Color, Rectangle, Point, GraphicException
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from math import sqrt
+
+
 
 
 kivy.require('1.8.0')
@@ -19,38 +18,26 @@ class UMLPainterCanvas(BoxLayout):
         super(self.__class__, self).__init__()
         
     
-    def on_touch_up(self, touch):
-        with self.canvas:
-            print("Podigo si prst")
-            return True
-         
-    def on_touch_move(self, touch):
-        with self.canvas:
-            print("Pomerio si prst")
-            return True
-    def on_touch_down(self, touch):
-        with self.canvas:
-            print("Spustio si prst")
-            return True
+    def metoda1(self):
+        
+        print("Kliknuo si na dugme 1")
+        
+    def metoda2(self):
+        print("Kliknuo si na dugme 2")
+        
+    
+    def metoda3(self):
+        print("Kliknuo si na dugme 3")
+        
+    
+    def metoda4(self):
+        print("Kliknuo si na dugme 4")
+        
+
     def metoda(self):
-        print("Usao u metodu")
+        print("Kliknuo si na canvas")
 
       
-#metoda za racunanje tacaka - nalazi se van klase       
-def calculate_points(x1, y1, x2, y2, steps=5):
-    dx = x2 - x1
-    dy = y2 - y1
-    dist = sqrt(dx * dx + dy * dy)
-    if dist < steps:
-        return None
-    o = []
-    m = dist / steps
-    for i in range(1, int(m)):
-        mi = i / m
-        lastx = x1 + dx * mi
-        lasty = y1 + dy * mi
-        o.extend([lastx, lasty])
-    return o
 
 
         
