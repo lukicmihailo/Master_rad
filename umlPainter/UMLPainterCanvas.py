3d5af3bf8f93db6a23b8840935c8a4abd9a196dd
Mihailo Lukic <lukicmihailo@gmail.com>
2014-03-23 21:29:28 +0100

    Verzija za profesora

--- umlPainter/UMLPainterCanvas.py
+++ umlPainter/UMLPainterCanvas.py
@@ -5,11 +5,11 @@ Created on Mar 2, 2014
 '''
 import kivy
 from kivy.uix.boxlayout import BoxLayout
-
-
+from kivy.uix.floatlayout import FloatLayout
 
 
 kivy.require('1.8.0')
+
 
 # Klasa predstavlja canvas na kome ce se crtati
 class UMLPainterCanvas(BoxLayout):
@@ -21,21 +21,29 @@ class UMLPainterCanvas(BoxLayout):
     def metoda1(self):
         
         print("Kliknuo si na dugme 1")
+        return True
         
     def metoda2(self):
         print("Kliknuo si na dugme 2")
-        
+        return True
     
     def metoda3(self):
         print("Kliknuo si na dugme 3")
-        
+        return True
     
     def metoda4(self):
         print("Kliknuo si na dugme 4")
-        
+        return True
+    
+    def metoda(self,touch):
+        with self.canvas.before:
+            floatLayout = self.ids["canvasID"]
+            if floatLayout.collide_point(touch.x, touch.y) == True:
+                print("Kliknuo si na canvas")
+                return True
 
-    def metoda(self):
-        print("Kliknuo si na canvas")
+   
+
 
       
 
