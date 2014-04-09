eafa2d59e3f8a2feeb5a91253d81dca3bf706858
Mihailo Lukic <lukicmihailo@gmail.com>
2014-04-09 23:01:32 +0200

    Inicijalna verzija projekta

--- main.py
+++ main.py
@@ -6,17 +6,22 @@ Created on Feb 22, 2014
 #!/usr/bin/kivy
 import kivy
 from kivy.app import App
-
-
-from umlPainter.UMLPainterCanvas import UMLPainterCanvas
+from kivy.lang import Builder
+from kivy.uix.anchorlayout import AnchorLayout
 
 kivy.require('1.8.0')
 
+Builder.load_file('toolbox.kv')
+Builder.load_file('drawingcanvas.kv')
+Builder.load_file('toolbar.kv')
+Builder.load_file('statusbar.kv')
+class UMLPainterFrame(AnchorLayout):
+    pass
+
 class UMLPainterApp(App):
     title = 'UMLPainterApp'
-
     def build(self):
-        return UMLPainterCanvas()
+        return UMLPainterFrame()
 
 if __name__ == '__main__':
     UMLPainterApp().run()
